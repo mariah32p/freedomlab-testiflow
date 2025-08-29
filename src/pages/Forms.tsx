b836c2ff5bcc386d222f777cde3fc91c0428f392
mariah32p <mariah@32parallels.com>
2025-08-29 18:17:23 -0500

    Updated Forms.tsx

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -370,7 +370,7 @@ export const Forms: React.FC = () => {
                           <span>Copy Link</span>
                         </button>
                         <a
-                          href={`/form/${form.id}`}
+                          href={`/submit/${form.id}`}
                           target="_blank"
                           rel="noopener noreferrer"
                           className="p-2 text-gray-400 hover:text-gray-600 transition-colors"
