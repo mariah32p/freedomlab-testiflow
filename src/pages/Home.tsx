93cc0ecb752222cd75a55f09d647ec98e4b4563d
mariah32p <mariah@32parallels.com>
2025-08-31 22:59:51 -0500

    Fix JSX parsing error by removing problematic comment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -149,7 +149,6 @@ export const Home: React.FC = () => {
         </div>
       </div>
 
-      {/* Collection Mockup Section */}
       <div className="py-24 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid lg:grid-cols-2 gap-16 items-center">
