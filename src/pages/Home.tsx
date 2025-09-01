f876f687f1089abfc3935b5f392cce0a6fa0c621
mariah32p <mariah@32parallels.com>
2025-08-31 23:45:48 -0500

    Remove the remaining Interactive Demo badge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -186,11 +186,6 @@ export const Home: React.FC = () => {
             {/* Demo Content */}
             <div className="relative h-[600px] sm:h-[700px] lg:h-[800px] overflow-hidden">
               <Demo />
-              
-              {/* Demo Overlay Indicator */}
-              <div className="absolute top-2 right-2 bg-blue-600 text-white px-2 py-1 rounded-full text-xs font-medium shadow-lg">
-                Interactive Demo
-              </div>
             </div>
           </div>
           
